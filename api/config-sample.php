424a8db79c54bfc9dce6878c79f75c7c87bbb944
shan <ahmedshan16@gmail.com>
2024-09-05 09:46:17 +0500

    added new config

--- api/config-sample.php
+++ api/config-sample.php
@@ -1,6 +1,7 @@
 <?php 
 return [
+    'NOTION_SECRET' => '',
     'NOTION_API' => 'https://api.notion.com/v1',
     'NOTION_VERSION' => '2022-02-22',
-    'DATABASE_ID' => '946f1fc79c3e463b90749ac5ef064182'
+    'DATABASE_ID' => ''
 ];
